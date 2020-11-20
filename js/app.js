27440390746073cf0b379efa276ff64b3024ae89
Jelson J <jelson20outlook.com>
2020-11-20 18:30:25 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -2,25 +2,24 @@
 //  JSON Data Parsing Nested in JavaScript
 let employes = `{
   "list": {
-      "name": "Harry Potter", "age": 25, "ocupatio": "design", "gender": "male",
-      "name": "Ana Maria", "age": 45, "ocupatio": "developer", "gender": "female",
-      "name": "Phil Pioter", "age": 19, "ocupatio": "tester", "gender": "male",
-      "name": "Harry Potter", "age": 18, "ocupatio": "tester", "gender": "male",
-      "name": "April Jackson", "age": 37, "ocupatio": "tester", "gender": "female",
-      "name": "Harry Potter", "age": 29, "ocupatio": "tester", "gender": "male",
-      "name": "Brad Poter", "age": 17, "ocupatio": "tester", "gender": "male",
-      "name": "Betty Paula", "age": 19, "ocupatio": "account", "gender": "female"
+      "name": "Harry Potter", "age": 25, "occupatio": "design", "gender": "male",
+      "name": "Ana Maria", "age": 45, "occupatio": "developer", "gender": "female",
+      "name": "Phil Pioter", "age": 19, "occupatio": "tester", "gender": "male",
+      "name": "Harry Potter", "age": 18, "occupatio": "tester", "gender": "male",
+      "name": "April Jackson", "age": 37, "occupatio": "tester", "gender": "female",
+      "name": "Harry Potter", "age": 29, "occupatio": "tester", "gender": "male",
+      "name": "Brad Poter", "age": 17, "occupatio": "fullstack developer", "gender": "male",
+      "name": "Betty Paula", "age": 19, "occupation": "account", "gender": "female"
     
   }
 
   
 }`
-
 ;
 
 let empList = JSON.parse(employes)
 console.log('------------JSON Data Parsing Nested in JavaScript--------------------')
-console.log(staffList)
+console.log(empList)
 
 
 
@@ -31,25 +30,25 @@ console.log('------------JavaScript Parsing Nested i Json--------------------')
 
 // JavaScript Parsing Nested i Json
 let staff = [
- {"name": "Harry Potter","ocupation": "tester","age": 19,
+ {"name": "Harry Potter","occupation": "tester","age": 19,
   "gender": "male"},
-  {"name": "Ane Jane","ocupation": "design","age": 25,
+  {"name": "Ane Jane","occupation": "design","age": 25,
   "gender": "female"},
-  {"name": "Ben Harry","ocupation": "web developer","age": 38,
+  {"name": "Ben Harry","occupation": "web developer","age": 38,
   "gender": "male"},
-  {"name": "Emma Ash","ocupation": "tester","age": 30,
+  {"name": "Emma Ash","occupation": "tester","age": 30,
   "gender": "female"},
-  {"name": "Peter Thomas","ocupation": "manage","age": 48,
+  {"name": "Peter Thomas","occupation": "back-end","age": 48,
   "gender": "male"},
-  {"name": "Andy Harry","ocupation": "developer","age": 33,
+  {"name": "Andy Harry","occupation": "front-end","age": 33,
   "gender": "male"},
-  {"name": "Jennifer Smith","ocupation": "account","age": 50,
+  {"name": "Jennifer Smith","occupation": "fullstack develope","age": 50,
   "gender": "female"},
-  {"name": "John Jackson","ocupation": "manage","age": 40,
+  {"name": "John Jackson","occupation": "back-end","age": 40,
   "gender": "male"},
-  {"name": "Ryan T","ocupation": "developer","age": 17,
+  {"name": "Ryan T","occupation": "fullstack develope","age": 17,
   "gender": "male"},
-  {"name": "Jenn Beck","ocupation": "tester","age": 52,
+  {"name": "Jenn Beck","occupation": "tester","age": 52,
   "gender": "female"},
 
 ];
