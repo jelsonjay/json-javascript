c539e1c5ccf9c4c757ea47c0b1984d3cda8d4dcc
Jelson J <jelson20outlook.com>
2020-11-20 18:00:23 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var json = `{
+
+//  JSON Data Parsing Nested in JavaScript
+let json = `{
   "book": {
       "name": "Harry Potter and the Goblet of Fire",
       "author": "J. K. Rowling",
@@ -11,21 +13,39 @@ var json = `{
   }
 }`;
 
+let compList = JSON.parse(json)
+console.log('------------JSON Data Parsing Nested in JavaScript--------------------')
+console.log(compList)
+
+
+
+
+
+
+console.log('------------JavaScript Parsing Nested i Json--------------------')
 
 // JavaScript Parsing Nested i Json
 let staff = [
- {"name": "Harry Potter","ocupation": "tester","age": 20,
-  "gender": "Fantasy Fiction"},
+ {"name": "Harry Potter","ocupation": "tester","age": 19,
+  "gender": "male"},
+  {"name": "Ane Jane","ocupation": "design","age": 25,
+  "gender": "female"},
+  {"name": "Harry Potter","ocupation": "tester","age": 38,
+  "gender": "male"},
+  {"name": "Harry Potter","ocupation": "tester","age": 30,
+  "gender": "male"},
+  {"name": "Harry Potter","ocupation": "tester","age": 48,
+  "gender": "male"},
+  {"name": "Harry Potter","ocupation": "tester","age": 33,
+  "gender": "male"},
+  {"name": "Jennifer Smith","ocupation": "account","age": 50,
+  "gender": "female"}
 
-{"paperback": "$10.40", "hardcover": "$20.32", "kindle": "$4.11"}
-  
 ];
 
 
-let compList = JSON.parse(json)
-console.log(compList)
 
-console.log('--------------------------------')
+
 
 
 
