8ecf0f49202232f21d36fb3fe99c94326d6d0a4d
Jelson J <jelson20outlook.com>
2020-11-20 17:54:13 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
-let companies = `{
+var json = `{
   "book": {
-      "name": "Jennifer Smith", "ocupation": "tester","age": 20,
-      "gender": "female"
-    },
-      {
-        "name": "Harry Peter", "ocupation": "design","age": 32,
-        "gender": "male"
-      },
-        {
-          "name": "Anne Jane", "ocupation": "account","age": 19,
-          "gender": "female"
-      },
-      {
-        "name": "Harry Potter", "ocupation": "web developer","age": 30,
-        "gender": "male"
-    }
+      "name": "Harry Potter and the Goblet of Fire",
+      "author": "J. K. Rowling",
+      "year": 2000,
+      "characters": ["Harry Potter", "Hermione Granger", "Ron Weasley"],
+      "genre": "Fantasy Fiction",
+      "price": 
 
- 
+      {"paperback": "$10.40", "hardcover": "$20.32", "kindle": "$4.11"}
   }
 }`;
 
-console.log(companies)
 
+// JavaScript Parsing Nested i Json
+let staff = [
+ {"name": "Harry Potter","ocupation": "tester","age": 20,
+  "gender": "Fantasy Fiction"},
+
+{"paperback": "$10.40", "hardcover": "$20.32", "kindle": "$4.11"}
+  
+];
+
+
+let compList = JSON.parse(json)
+console.log(compList)
+
+console.log('--------------------------------')
+
+
+
+let staffList = JSON.stringify(staff)
+console.log(staffList)
